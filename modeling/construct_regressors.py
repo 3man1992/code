467bfcb2d1902392a308019137cbb7593a00db6f
Laurence Freeman <laurence.freeman@live.co.uk>
2021-02-06 09:45:43 +0000

    Updated choices to only include good trials reducing no. observations from 12,134 to 10608 Set right choice to one with left choice to 0 as the input requires a binary success input

--- modeling/construct_regressors.py
+++ modeling/construct_regressors.py
@@ -6,15 +6,18 @@ def construct_regressors(data,nBack):
     regressor_cxr = np.zeros((1,nBack))
     regressor_rew = np.zeros((1,nBack))
     nChoices = len(data)
-    regressors = []
+    regressors = np.zeros((nChoices,nBack * 3))
 
-    #Update regregressors
+    # Update regregressors
     for choice in range(nChoices):
         #Which side was picked
         side = data.at[choice,'sides']
 
         #Fill in the regressor
-        regressors.insert(choice, [regressor_cxr, regressor_ch, regressor_rew])
+        # regressors.insert(choice, [regressor_cxr, regressor_ch, regressor_rew])
+        regressors[choice,0:10] = regressor_cxr
+        regressors[choice,10:20] = regressor_ch
+        regressors[choice,20:30] = regressor_rew
 
         """% These letter variables will be one iff the choice_i'th letter is their letter.
         Set them to zero, so that if it's a violation trial and they don't get set,
@@ -27,6 +30,9 @@ def construct_regressors(data,nBack):
         x = 0
         r = 0
 
+        """For the regessors you ultimately need binary values. 1 and -1 is used if __name__ == '__main__':
+            preference due to multiplication of 0 being difficult. c is arbitarily set as left as -1 and right as 1.
+            Reward is of course given as 1 and omission as 0. And x is the product of c and r"""
         if side == 'l':
             if reward == 1:
                 c = -1
@@ -51,8 +57,11 @@ def construct_regressors(data,nBack):
             continue
 
         #Add regressor to start of vector and remove last value
-        regressor_ch = np.insert(regressor_ch, 0, c)
+        regressor_ch = np.insert(regressor_ch, 0,c)
+        regressor_ch = regressor_ch[:-1]
         regressor_cxr = np.insert(regressor_cxr, 0, x)
+        regressor_cxr = regressor_cxr[:-1]
         regressor_rew = np.insert(regressor_rew, 0, r)
+        regressor_rew = regressor_rew[:-1]
 
     return(regressors)
--- modeling/main.py
+++ modeling/main.py
@@ -1,31 +1,57 @@
 #External imports
 import statsmodels.api as sm
-import numpy as np
 from statsmodels.formula.api import glm
+import pandas as pd
+import numpy as np
+
+# pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 #My imports
 from import_data import load_data
 from construct_regressors import construct_regressors
 
-# pd.set_option("display.max_rows", None, "display.max_columns", None)
-
-#Create regressors
+#LOAD DATA
 rat_data_file = "/Users/laurence/Desktop/Neuroscience/mproject/data/modeling_workflow_example/ratdata.mat"
 data = load_data(rat_data_file)
+
+"""Choices / Code to convert choices into binary to solve concat error"""
+"""Only analyze trials that are non-vilations and of free choice."""
+good_trials = data.loc[(data["sides"] == "l") | (data["sides"] == "r")]
+good_trials = data.loc[(data["trial_types"] == "f")]
+choices = np.asarray(good_trials["sides"])
+for choice in range(len(choices)):
+    if choices[choice] == "l":
+        choices[choice] = 0
+    elif choices[choice] == "r":
+        choices[choice] = 1
+    elif choices[choice] == "v": #Will need to remove this code
+        choices[choice] = 0
+        print("Error, violation trials should have been removed")
+    else:
+        print("Error, something happened that did not adhere to the choice logic", choices[choice])
+choices = np.array(choices, dtype=np.float)
+
+"""Reduce the regressors to only include the good trials"""
 regressors = construct_regressors(data, 10)
-
-#Outline inputs to GLM
+# regessors = regressors.astype(int)
+regressors = pd.DataFrame(regressors)
+index_of_good_trials = good_trials.index.values
+regressors = regressors[regressors.index.isin(good_trials.index)]
 X = sm.add_constant(regressors, prepend=False)
-choices = np.asarray(data["sides"])
-assert len(choices) == 12324, "Choice length not as expected"
 
-#Set family for GLM model
-link_function = sm.families.links.logit
-model_family = sm.families.Binomial(link = link_function)
-
-#GLM functions
-model = sm.GLM(choices, X, model_family)
-results = model.fit()
+"""GLM functions"""
+model = sm.GLM(choices, X, family = sm.families.Binomial()).fit()
 print(model.summary())
 
-#hello
+"""Data exploration"""
+#Explore data types
+# print("")
+# print("Choices")
+# print("-------")
+# print("Choices is of type:", type(choices))
+# print("Choices if of dtype:", choices.dtype)
+# print("")
+# print("Regressors")
+# print("-------")
+# print("Regressors is of type:", type(regressors))
+# print("Regressors is of type:", regressors.dtype)
